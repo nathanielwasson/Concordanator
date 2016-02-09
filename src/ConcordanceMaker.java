acf6063ebaebfed1aba0be75f348b4ea6bc3c62c
Anthony Ratliff <arratlif@uncg.edu>
2016-02-09 10:36:19 -0500

    Changed my main method to now start the CmdRepl class.  All written methods are currently working fine in Windows OS.  Still issues with Linux.

--- src/ConcordanceMaker.java
+++ src/ConcordanceMaker.java
@@ -1,10 +1,10 @@
 
-import ClassLibrary.Bookshelf;
-import ClassLibrary.Concordance;
-import java.util.Scanner;
+import ClassLibrary.CmdRepl;
+import java.io.IOException;
 
 public class ConcordanceMaker{
     public static void main(String[] args) {
+        /*
         Bookshelf myBooks = new Bookshelf();
         System.out.println("The number of books are: " + myBooks.getNumberOfBooks());
         String[] bookTitle = myBooks.getAllBookTitles();
@@ -22,6 +22,27 @@ public class ConcordanceMaker{
         searchTerm = search.nextLine();
         String[] bookCreds = myBooks.pullBook(searchTerm);
         Concordance concord = new Concordance(bookCreds[0], bookCreds[1], bookCreds[2]);
-        System.out.println(concord.makeConcordance());
+        System.out.println(concord.makeConcordance());*/
+        
+        String[] start = {"Hello"};
+        CmdRepl repl = new CmdRepl(start);
+        
+        // Test out get all book titles
+        
+        
+        
+        try {
+            
+            if (args.length != 0 && args[1].equals("gui")) {
+                // Launch the gui from here
+            } else if (args.length == 1 || args.length == 0) {
+                repl.startRepl("");
+            } else {
+                // Print out usage to term
+            }            
+            
+        } catch (IOException ex) {
+            System.out.println("No File.");
+        }
     }
 }
