4c3a9e8203d80c82c0545c486e0987ea091a48d7
Anthony Ratliff <arratlif@uncg.edu>
2016-02-08 22:02:37 -0500

    Added the getters and setters to the Word class.  This concordance now actually builds a table of words based on a book passed into the constructor as three strings.  The three strings are passed in from the Bookshelf class method 'pullBook'.

--- src/ClassLibrary/Concordance.java
+++ src/ClassLibrary/Concordance.java
@@ -16,14 +16,14 @@ public class Concordance implements Serializable {
     // private class fields.
     LinkedList[] hastable;
     String bookTitle, bookAuthor, filePath;
-    int bookStartLine;
+    int wordCount;
     
     public Concordance(String bt, String a, String fn) {
         this.hastable = new LinkedList[27];
         this.bookTitle = bt;
         this.bookAuthor = a;
         this.filePath = fn;
-        this.bookStartLine = 0;
+        this.wordCount = 0;
     }
     
     public boolean makeConcordance(){
@@ -54,16 +54,21 @@ public class Concordance implements Serializable {
                                 if (word.length() > 0){
                                 char firstChar = word.charAt(0);
                                 hashKey = (((int)firstChar % 97) + 1);
-                                if (hashKey < 28){
-                                System.out.println(hashKey);
+                                if (hashKey > 27) hashKey = 0;
                                 if (this.hastable[hashKey] == null){
                                     this.hastable[hashKey] = new LinkedList();
                                 }
                                 Word addWord = new Word(word);
+                                if (this.hastable[hashKey].contains(addWord.getWord())){
+                                    
+                                }
+                                else{
                                 this.hastable[hashKey].add(addWord);
+                                }
+                                this.wordCount++;
                                 System.out.println("Hash Key = " + hashKey);
                                 System.out.println("Word = " +word);
-                            }
+                            
                             }
                             }
                         }
@@ -76,13 +81,12 @@ public class Concordance implements Serializable {
                     else {
                         if (line.contains("*** START OF THIS PROJECT GUTENBERG")) {
                             bookStart = true;
-                            this.bookStartLine = fileLineNumber + 1;    //No longer really need this..?
                         }
                     }
                 }
                 System.out.println(bookTitle + " " + bookAuthor + " " + filePath);
                 System.out.println("Total line numbers of text file: " + fileLineNumber);
-                System.out.println("Book begins on line: " + this.bookStartLine);
+                System.out.println("Total number of words found: " + this.wordCount);
                 success = true;
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(Concordance.class.getName()).log(Level.SEVERE, null, ex);
@@ -101,11 +105,31 @@ public class Concordance implements Serializable {
         ArrayList<Integer> line_numbers;
         int number_apperances, apperance_rank;
         
-        public Word(String w){
+        private Word(String w){
             this.word = w;
-            this.line_numbers = null;
+            this.line_numbers = new ArrayList<>();
             this.number_apperances = 1;
             this.apperance_rank = 0;
+        }
+        
+        private String getWord(){
+            return this.word;
+        }
+        
+        private ArrayList<Integer> getLineNumbers(){
+            return this.line_numbers;
+        }
+        
+        private void addLineNumber(int num){
+            this.line_numbers.add(num);
+        }
+        
+        private void incrementWordCount(){
+            this.number_apperances++;
+        }
+        
+        private int getWordRank(){
+            return this.apperance_rank;
         }
     }
 }
