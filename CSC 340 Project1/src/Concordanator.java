13f5ca4d60d91631c9e5a62213743ae84570ad61
Anthony Ratliff <arratlif@uncg.edu>
2016-02-19 16:36:58 -0500

    Creating new Project again.

---
+++ CSC 340 Project1/src/Concordanator.java
@@ -0,0 +1,26 @@
+
+import ClassLibrary.CmdRepl;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Concordanator{
+    public static void main(String[] args) {
+   // Start the repl from here
+        CmdRepl repl = new CmdRepl();
+        
+        try {
+            
+            if (args.length != 0 && args[1].equals("gui")) {
+                // Launch the gui from here
+            } else if (args.length == 1 || args.length == 0) {
+                repl.startRepl("");
+            } else {
+                // Print out usage to term
+            }            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Concordanator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+}
